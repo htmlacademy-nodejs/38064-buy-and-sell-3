022ccc2d4a742677b12a59602fc0a329443006b0
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2020-09-22 16:49:20 +0300

    fix routes

--- src/express/routes/index.js
+++ src/express/routes/index.js
@@ -5,10 +5,10 @@ const {Router} = require(`express`);
 
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.send(`/`));
-mainRouter.get(`/register`, (req, res) => res.send(`/register`));
 mainRouter.get(`/login`, (req, res) => res.send(`/login`));
+mainRouter.get(`/register`, (req, res) => res.send(`/register`));
 mainRouter.get(`/search`, (req, res) => res.send(`/search`));
+mainRouter.get(`/`, (req, res) => res.send(`/`));
 
 
 module.exports = mainRouter;
