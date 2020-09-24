9eb1519ee7f5960a3d2ba68c0bcd2f5025660685
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2020-09-24 04:38:23 +0300

    Обновляет шаблоны страниц

--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -5,10 +5,10 @@ const {Router} = require(`express`);
 
 const offersRouter = new Router();
 
-offersRouter.get(`/add`, (req, res) => res.render(`new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`ticket-edit`));
-offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
-offersRouter.get(`/:id`, (req, res) => res.render(`ticket`));
+offersRouter.get(`/add`, (req, res) => res.render(`offers/new-ticket`));
+offersRouter.get(`/edit/:id`, (req, res) => res.render(`offers/ticket-edit`));
+offersRouter.get(`/category/:id`, (req, res) => res.render(`offers/category`));
+offersRouter.get(`/:id`, (req, res) => res.render(`offers/ticket`));
 
 
 module.exports = offersRouter;
