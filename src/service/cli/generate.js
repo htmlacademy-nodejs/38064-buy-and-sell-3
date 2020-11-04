7a1a2369ca480188c4435f09bf8fe9eada698f1f
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2020-11-04 20:27:47 +0300

    Обновляет структуру данных для объявлений

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -2,6 +2,7 @@
 
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
+const {nanoid} = require(`nanoid`);
 const {ExitCode} = require(`../../utils/const`);
 const {getRandomInt, shuffleArray} = require(`../../utils/common`);
 
@@ -9,6 +10,8 @@ const {getRandomInt, shuffleArray} = require(`../../utils/common`);
 const DEFAULT_COUNT = 1;
 const MAX_COUNT = 1000;
 const FILE_NAME = `mocks.json`;
+const ID_LENGTH = 6;
+const MAX_COMMENT_COUNT = 5;
 
 const SumRestrict = {
   MIN: 1000,
@@ -34,6 +37,7 @@ const ContentFile = {
   TITLES: `./data/titles.txt`,
   CATEGORIES: `./data/categories.txt`,
   SENTENCES: `./data/sentences.txt`,
+  COMMENTS: `./data/comments.txt`,
 };
 
 
@@ -46,24 +50,39 @@ const getPictureFileName = (number) => {
 };
 
 /**
+ * @param {string[]} sentences
+ * @return {Comment[]}
+ */
+const generateComments = (sentences) => {
+  const count = getRandomInt(1, MAX_COMMENT_COUNT);
+  return Array(count).fill({}).map(() => ({
+    id: nanoid(ID_LENGTH),
+    text: shuffleArray(sentences.slice()).slice(0, getRandomInt(1, sentences.length)).join(` `),
+  }));
+};
+
+/**
  * @param {number} count
  * @param {string[]} titles
  * @param {string[]} sentences
  * @param {string[]} categories
+ * @param {string[]} comments
  * @return {Offer[]}
  */
-const generateOffers = (count = DEFAULT_COUNT, titles, sentences, categories) => {
+const generateOffers = (count = DEFAULT_COUNT, titles, sentences, categories, comments) => {
   return Array(count).fill({}).map(() => {
     const descriptionSentencesCount = getRandomInt(DescriptionRestrict.MIN, DescriptionRestrict.MAX);
     const categoriesCount = getRandomInt(1, categories.length);
 
     return {
+      id: nanoid(ID_LENGTH),
       title: titles[getRandomInt(0, titles.length - 1)],
       description: shuffleArray(sentences).slice(0, descriptionSentencesCount).join(` `),
       category: shuffleArray(categories).slice(0, categoriesCount),
       picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
       type: Object.values(OfferType)[getRandomInt(0, 1)],
       sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
+      comments: generateComments(comments),
     };
   });
 };
@@ -86,10 +105,11 @@ const readContent = async (filePath) => {
  * @param {string[]} args
  */
 const run = async (args) => {
-  const [titles, sentences, categories] = await Promise.all([
+  const [titles, sentences, categories, comments] = await Promise.all([
     readContent(ContentFile.TITLES),
     readContent(ContentFile.SENTENCES),
     readContent(ContentFile.CATEGORIES),
+    readContent(ContentFile.COMMENTS),
   ]);
 
   const [count] = args;
@@ -100,7 +120,7 @@ const run = async (args) => {
     process.exit(ExitCode.ERROR);
   }
 
-  const content = JSON.stringify(generateOffers(countOffer, titles, sentences, categories));
+  const content = JSON.stringify(generateOffers(countOffer, titles, sentences, categories, comments));
 
   try {
     await fs.writeFile(FILE_NAME, content, `utf-8`);
@@ -120,11 +140,19 @@ module.exports = {
 
 
 /**
+ * @typedef {Object} Comment
+ * @property {string} id
+ * @property {string} text
+ */
+
+/**
  * @typedef {Object} Offer
+ * @property {string} id
  * @property {string} title
  * @property {string} description
- * @property {string} category
+ * @property {string[]} category
  * @property {string} picture
- * @property {OfferType.OFFER|OfferType.SALE} type
+ * @property {OfferType} type
  * @property {number} sum
+ * @property {Comment[]} comments
  */
